83d0c23740384475fdcedf3e62a5d29691387fa9
katai5plate <katai5plate.sub@gmail.com>
2020-09-27 21:53:26 +0900

    don't get time every time

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var APP_NAME = "NOLA-MOCK-DATE";
 var LOCAL_STORAGE_OFFSET_KEY = `${APP_NAME}_offset`;
 
-var code = function (APP_NAME, offset) {
+var code = function ({ APP_NAME, LOCAL_STORAGE_OFFSET_KEY }) {
   console.log(`${APP_NAME}: start!`);
-  if (!offset) {
-    return console.warn(
-      `${APP_NAME}: Getting normal time now. Please reload after a while!`
+  const offset = Number(localStorage.getItem(LOCAL_STORAGE_OFFSET_KEY));
+  if (!offset || location.hash === "#remock") {
+    console.warn(
+      `${APP_NAME}: Getting normal time now. Please erase the hash and reload after a while!`
     );
+    console.log(`${APP_NAME}: fetching...`);
+    return fetch("//worldtimeapi.org/api/ip")
+      .then((x) => x.json())
+      .then((x) => {
+        const offset = x.unixtime * 1000 - Date.now();
+        localStorage.setItem(LOCAL_STORAGE_OFFSET_KEY, offset);
+        console.log(
+          `${APP_NAME}: Got normal time -> ${new Date(Date.now() + offset)}`
+        );
+      });
   }
   console.log(`${APP_NAME}: offset -> ${offset}`);
   const _date = Date;
@@ -22,16 +33,7 @@ var code = function (APP_NAME, offset) {
   console.log(`${APP_NAME}: done!`);
 };
 
-fetch("//worldtimeapi.org/api/ip")
-  .then((x) => x.json())
-  .then((x) => {
-    const offset = x.unixtime * 1000 - Date.now();
-    localStorage.setItem(LOCAL_STORAGE_OFFSET_KEY, offset);
-    console.log(
-      `${APP_NAME}: Got normal time -> ${new Date(Date.now() + offset)}`
-    );
-  });
-
-location.href = `javascript: (${code.toString()})("${APP_NAME}",${localStorage.getItem(
-  LOCAL_STORAGE_OFFSET_KEY
-)})`;
+location.href = `javascript: (${code.toString()})(${JSON.stringify({
+  APP_NAME,
+  LOCAL_STORAGE_OFFSET_KEY,
+})})`;
