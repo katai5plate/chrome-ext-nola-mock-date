0122c402b3fad067e06060a237f28a5740e92acd
katai5plate <katai5plate.sub@gmail.com>
2020-09-27 20:52:29 +0900

    thank you for tig

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello!");
+var code = function () {
+  const MINUTES = 1000 * 60;
+  const DATE_OFFSET = -15 * MINUTES;
+
+  console.log("mocking...");
+
+  const _date = Date;
+  window.Date = function (...args) {
+    console.log(args);
+    if (args.length) {
+      return new _date(...args);
+    }
+    return new _date(Date.now() + DATE_OFFSET);
+  };
+  for (const i of Object.getOwnPropertyNames(_date)) {
+    window.Date[i] = _date[i];
+  }
+
+  console.log("done!");
+};
+location.href = `javascript: (${code.toString()})()`;
